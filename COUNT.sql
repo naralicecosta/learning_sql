3f87685868c655f283765ff95ce7942518e77f1c
naralicecosta <naralicecosta04@gmail.com>
2024-03-20 18:25:16 -0300

    count

---
+++ COUNT.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(*) 
+FROM person.person
+
+SELECT CONT (DISTINCT title)
+FROM person.person
+
+SELECT COUNT(*)
+FROM Production.Product
+
+SELECT COUNT (Size)
+FROM Production.Product
+
+SELECT COUNT(DISTINCT Size)
+FROM Production.Product
